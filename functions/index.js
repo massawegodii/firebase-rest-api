cc37898e019db92cb5404d816d7da99a8977f3d3
massawegodii <massawegodfrey43@gmail.com>
2025-03-27 15:50:31 +0300

    mofifications

--- functions/index.js
+++ functions/index.js
@@ -57,70 +57,68 @@ app.get("/api/get/:id", (req, res) => {
   })();
 });
 
-// // read all user details
-// // get
-// app.get("/api/userDetails", (req, res) => {
-//   (async () => {
-//     try {
-//       let query = db.collection("userdetails");
-//       let response = [];
+// // Read all user details
+app.get("/api/get/all", async (req, res) => {
+    try {
+      const querySnapshot = await db.collection("userdetails").get();
+  
+      if (querySnapshot.empty) {
+        return res.status(200).send({ status: "Success", data: [] });
+      }
 
-//       await query.get().then((data) => {
-//         let docs = data.docs; // query results
+      const response = [];
+  
+      querySnapshot.forEach((doc) => {
+        console.log("Document Data:", doc.data());
+  
+        const data = doc.data();
+        response.push({
+          name: data.name || null,
+          mobile: data.mobile || null,
+          address: data.address || null,
+        });
+      });
+  
+      res.status(200).send({ status: "Success", data: response });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ status: "Failed", msg: error.message });
+    }
+  });
+  
+  
 
-//         docs.map((doc) => {
-//           const selectedData = {
-//             name: doc.data().name,
-//             mobile: doc.data().mobile,
-//             address: doc.data().address,
-//           };
+ // update
+app.put("/api/update/:id", (req, res) => {
+  (async () => {
+    try {
+      const reqDoc = db.collection("userdetails").doc(req.params.id);
+      await reqDoc.update({
+        name: req.body.name,
+        mobile: req.body.mobile,
+        address: req.body.address,
+      });
+      return res.status(200).send({ status: "Success", msg: "Data Updated" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ status: "Failed", msg: error });
+    }
+  })();
+});
 
-//           response.push(selectedData);
-//         });
-//         return response;
-//       });
-
-//       return res.status(200).send({ status: "Success", data: response });
-//     } catch (error) {
-//       console.log(error);
-//       res.status(500).send({ status: "Failed", msg: error });
-//     }
-//   })();
-// });
-
-// // update
-// // put
-// app.put("/api/update/:id", (req, res) => {
-//   (async () => {
-//     try {
-//       const reqDoc = db.collection("userdetails").doc(req.params.id);
-//       await reqDoc.update({
-//         name: req.body.name,
-//         mobile: req.body.mobile,
-//         address: req.body.address,
-//       });
-//       return res.status(200).send({ status: "Success", msg: "Data Updated" });
-//     } catch (error) {
-//       console.log(error);
-//       res.status(500).send({ status: "Failed", msg: error });
-//     }
-//   })();
-// });
-
-// // delete
-// // delete
-// app.delete("/api/delete/:id", (req, res) => {
-//   (async () => {
-//     try {
-//       const reqDoc = db.collection("userdetails").doc(req.params.id);
-//       await reqDoc.delete();
-//       return res.status(200).send({ status: "Success", msg: "Data Removed" });
-//     } catch (error) {
-//       console.log(error);
-//       res.status(500).send({ status: "Failed", msg: error });
-//     }
-//   })();
-// });
+// Delete Data
+app.delete("/api/delete/:id", (req, res) => {
+  (async () => {
+    try {
+      const reqDoc = db.collection("userdetails").doc(req.params.id);
+      await reqDoc.delete();
+      return res.status(200).send({ status: "Success", msg: "Data Removed" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ status: "Failed", msg: error });
+    }
+  })();
+});
 
 // Exports api to the firebase cloud functions
 exports.app = functions.https.onRequest(app);
